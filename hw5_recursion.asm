c23cb8f6a840360b2dc30bf77826ea5c2d071370
Nikita <72099127+NikitaRadionov@users.noreply.github.com>
2024-10-17 16:30:29 +0300

    Add files via upload

---
+++ hw5_recursion.asm
@@ -0,0 +1,42 @@
+.data
+	message: .asciz "! = "
+.text
+.main:
+	mv a0, zero # n
+	li a1, 1 # n!
+	jal max_fact
+	li a7, 1
+	ecall
+	
+	li a7, 4
+	la a0, message
+	ecall
+	
+	li a7, 1
+	mv a0, a1
+	ecall
+	
+	li a7, 10
+	ecall
+
+max_fact:
+	# push ra on stack
+	addi sp, sp, -4
+	sw ra, (sp)
+	
+	mv t0, a0
+	mv t1, a1
+	
+	addi t0, t0, 1 # n -> n + 1
+	mul t1, t1, t0 # n! -> (n + 1)!
+	div t2, t1, a1 # (n + 1)! / n!
+	
+	bne t2, t0, done # (n + 1) ! / n! == n + 1 ?
+	mv a0, t0
+	mv a1, t1
+	jal max_fact
+done:
+	lw ra, (sp)
+	addi sp, sp, 4
+	ret
+
