96d4184848b925515b213195fd2d376bc230c2c9
Nikita <72099127+NikitaRadionov@users.noreply.github.com>
2024-10-17 16:42:48 +0300

    Update hw5_loop.asm

--- hw5_loop.asm
+++ hw5_loop.asm
@@ -1,26 +1,39 @@
+.data
+	message: .asciz "! = "
 .text
 .main:
+	mv a0, zero # n
+	li a1, 1 # n!
 	jal max_fact
 	li a7, 1
 	ecall
+	
+	li a7, 4
+	la a0, message
+	ecall
+	
+	li a7, 1
+	mv a0, a1
+	ecall
+	
 	li a7, 10
 	ecall
 
 max_fact:
 	addi sp, sp, -4
 	sw ra, (sp)
-	mv t0, zero
-	li t1, 1
 max_fact_while:
+	mv t0, a0
+	mv t1, a1
 	mv t2, t1
-	addi t0, t0, 1
-	mul t1, t2, t0
-	div t3, t1, t2
-	bne t3, t0, max_fact_end_while
+	addi t0, t0, 1 # n -> n + 1
+	mul t1, t1, t0 # n! -> (n + 1)!
+	div t2, t1, a1 # (n + 1)! / n!
+	bne t2, t0, max_fact_end_while
+	mv a0, t0
+	mv a1, t1
 	j max_fact_while
 max_fact_end_while:
-	mv a0, t2
 	lw ra, (sp)
 	addi sp, sp, 4
 	ret
-
